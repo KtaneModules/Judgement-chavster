7856597d52d11157249b4401a324c24e56980274
chavster <aaronkenna2006@gmail.com>
2024-12-16 14:11:10 +0000

    More modelling, started programming

--- Assets/Judgement.cs
+++ Assets/Judgement.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 using KModkit;
 using Rnd = UnityEngine.Random;
 using Math = ExMath;
+using NUnit.Framework;
 
 public class Judgement : MonoBehaviour
 {
@@ -19,6 +20,13 @@ public class Judgement : MonoBehaviour
     [SerializeField] private TextMesh DisplayText;
     [SerializeField] private TextMesh NumberText;
 
+    private string[] Forenames = { "Aidan", "Chav", "Zoe", "Deaf", "Blan", "Ghost", "Hazel", "Goober", "Jimmy", "Homer", "Saul", "Walter", "Jeremiah", "Jams", "Rich-Hard", "Jo", "Johnny", "Dwayne", "Cave", "Burger", "Jerma", "Sans", "Jon", "Garfield", "Mega", "Cruel", "Cyanix", "Tim", "Bomby", "Edgework", "Complicated", "Jason", "Freddy", "Gaga", "Barry \"Bee\"", "Mordecai", "Rigby", "Jesus", "Seymour", "Superintendent", "Kevin", "dicey", "User", "Eltrick"  };
+    private string[] Surnames = { "Anas", "Salt", "Ster", "Blind", "Ante", "McBoatface", "McGooberson", "Neutron", "Simpleton", "Goodman", "White", "Clahkson", "Maie", "Hammock", "Ku", "Cage", "Rock-Johnson", "King", "985", "Tron", "Serif", "Master", "Wi", "McBombface", ".HandlePass();", "McEdgework", "Optimised", "Alfredo", "Voorhees", "Fazbear", "Oolala", "Benson", "Christ", "Skinner", "Lee", "Name" };
+    private string[] Crimes = { "Silliness", "Tax Fraud", "Dying", "Striking", "Solving", "Living", "Embezzlement", "Being Guilty", "Handling Salmon", "Minor Larceny", "{CRIME}", "Trolling", "Cringe on Main", "Said \"Fuck\" :c", "Bad at Balatro", "meanie :c", "Morbing", "araraarar", "Bad Romance", "Deaf and Blind", "Bees", "the", "Teleporting Bread", "Blasphemy", "Jiggy wit it", "Rap Battle", "Aurora Borealis", "Poker Face", "Party Rockin'", "Witchcraft", "Downloading a Car", "Naming" };
+    private int ChosenForename;
+    private int ChosenSurname;
+    private int ChosenCrime;
+    private Coroutine[] ButtonAnim;
 
     static int ModuleIdCounter = 1;
     int ModuleId;
@@ -29,20 +37,24 @@ public class Judgement : MonoBehaviour
         ModuleId = ModuleIdCounter++;
         GetComponent<KMBombModule>().OnActivate += Activate;
 
+        ChosenForename = Rnd.Range(0, Forenames.Length);
+        ChosenSurname = Rnd.Range(0, Surnames.Length);
+        ChosenCrime = Rnd.Range(0, Crimes.Length);
+        
     }
 
     void Activate()
-    { 
+    {
 
     }
 
     void Start()
-    { 
+    {
 
     }
 
     void Update()
-    { 
+    {
 
     }
 
